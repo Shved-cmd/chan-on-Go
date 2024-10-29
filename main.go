42e77cd8b728788217c47a23654b1609a4679d59
Shved-cmd <megajoker0@gmail.com>
2024-10-29 15:05:15 +0200

    Added entering and leaving notifications

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
+import "github.com/fatih/color"
+
 type client chan<- string // an outgoing message channel
 
 type clientInfo struct {
 	ch   client
 	name string
 }
+
+var (
+	entering = make(chan clientInfo)
+	leaving  = make(chan clientInfo)
+	messages = make(chan string) // all incoming client messages
+	green    = color.New(color.FgGreen).SprintfFunc()
+
+	//
+)
 
 func main() {
 
